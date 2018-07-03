498b641335fcd6193a9ccc2899d1d343718c4fa1
Nikolas Thitu <nikolas@hauz.io>
2018-07-03 15:52:31 -0400

    adding ability to sign builds; updating dependency versions to latest

--- bin/ci-setup-env.sh
+++ bin/ci-setup-env.sh
@@ -5,3 +5,6 @@ mkdir -p ~/.gradle
 echo "trusonaUsername=${ARTIFACTORY_USERNAME}" >> ~/.gradle/gradle.properties
 echo "trusonaPassword=${ARTIFACTORY_PASSWORD}" >> ~/.gradle/gradle.properties
 
+echo "signingKeyId=${SIGNING_KEY_ID}" >> ~/.gradle/gradle.properties
+echo "signingPassword=${SIGNING_PASSWORD}" >> ~/.gradle/gradle.properties
+echo "signingSecretKeyRingFile=${SIGNING_KEY_RING_FILE}" >> ~/.gradle/gradle.properties
