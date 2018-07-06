a0352f755ba1be7910728cbac77d740457b4ba56
Nikolas Thitu <nikolas@hauz.io>
2018-07-06 09:38:46 -0400

    updating release script

--- bin/release.sh
+++ bin/release.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
 
+tmp_file=`mktemp`
+
 if [ ! -f gradlew ]; then
   echo "Cannot find gradle wrapper. Run from repo root."
-  exit
+  exit -1
 fi
+
 
 SNAPSHOT_VERSION=$(./gradlew trusona-sdk:printVersion | grep SNAPSHOT)
 
 if [ "${SNAPSHOT_VERSION}" == "" ]; then
   echo "Version is not a snapshot. Aborting."
-  exit
+  exit -1
 fi
 
+
 GIT_STATUS=$(git status -s)
+
 if [ ! -z "${GIT_STATUS}" ]; then
   echo "Repo is not clean. Resolve the following uncomitted changes:"
   echo ${GIT_STATUS}
-  exit
+  exit -1
 fi
 
 
@@ -26,20 +31,23 @@ BRANCH_NAME="release-${RELEASE_VERSION}"
 TAG_NAME="v${RELEASE_VERSION}"
 
 git checkout -b ${BRANCH_NAME}
-sed -i '' "s/[0-9]*\.[0-9]*\.[0-9]-SNAPSHOT/${RELEASE_VERSION}/" build.gradle
+
+sed -e "s/${SNAPSHOT_VERSION}/${RELEASE_VERSION}/g" build.gradle > ${tmp_file}
+mv ${tmp_file} build.gradle
 git add build.gradle
+
 git commit -m "release ${TAG_NAME}"
-git tag -a ${TAG_NAME} -m "release ${TAG_NAME}"
+git tag -f -a ${TAG_NAME} -m "release ${TAG_NAME}"
 
 echo -n "Do you want to publish version ${RELEASE_VERSION} now? [y/N]: "
 read RESPONSE
 
 if [ "${RESPONSE}" == "y" ]; then
-  git push origin ${BRANCH_NAME}
-  git push origin ${TAG_NAME}
+  git push --set-upstream origin ${BRANCH_NAME}
+  git push --set-upstream origin ${TAG_NAME}
 else
-  echo "Run the following commands to publish release:"
-  echo ""
-  echo git push origin ${BRANCH_NAME}
-  echo git push origin ${TAG_NAME}
+  echo "Run the following commands to publish release:\n"
+  echo git push --set-upstream origin ${BRANCH_NAME}
+  echo git push --set-upstream origin ${TAG_NAME}
 fi
+
