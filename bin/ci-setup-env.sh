b33d2da87a738e2b784ebf01c7913fc46b5e14aa
Nikolas Thitu <nikolas@hauz.io>
2018-07-06 09:10:48 -0400

    updating ci env setup

--- bin/ci-setup-env.sh
+++ bin/ci-setup-env.sh
@@ -5,6 +5,9 @@ mkdir -p ~/.gradle
 echo "trusonaUsername=${ARTIFACTORY_USERNAME}" >> ~/.gradle/gradle.properties
 echo "trusonaPassword=${ARTIFACTORY_PASSWORD}" >> ~/.gradle/gradle.properties
 
+echo "ossrhUsername=${OSS_RH_USERNAME}" >> ~/.gradle/gradle.properties
+echo "ossrhPassword=${OSS_RH_PASSWORD}" >> ~/.gradle/gradle.properties
+
 echo "signingKeyId=${SIGNING_KEY_ID}" >> ~/.gradle/gradle.properties
 echo "signingPassword=${SIGNING_PASSWORD}" >> ~/.gradle/gradle.properties
 echo "signingSecretKeyRingFile=${SIGNING_KEY_RING_FILE}" >> ~/.gradle/gradle.properties
