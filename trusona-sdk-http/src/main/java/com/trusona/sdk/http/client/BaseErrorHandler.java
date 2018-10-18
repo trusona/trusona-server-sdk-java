91121ea229cd4c3e3c4884a5705b602ad87b0744
Nikolas Thitu <nikolas@hauz.io>
2018-10-17 23:51:07 -0400

    ensuring a case of null on errorBody() is does not blow up

--- trusona-sdk-http/src/main/java/com/trusona/sdk/http/client/BaseErrorHandler.java
+++ trusona-sdk-http/src/main/java/com/trusona/sdk/http/client/BaseErrorHandler.java
@@ -4,28 +4,30 @@ import com.trusona.sdk.config.JacksonConfig;
 import com.trusona.sdk.http.ErrorHandler;
 import com.trusona.sdk.http.client.v2.response.ErrorResponse;
 import com.trusona.sdk.resources.exception.TrusonaException;
+import okhttp3.ResponseBody;
 import retrofit2.Response;
 
 import java.io.IOException;
-import java.io.Reader;
+import java.util.Optional;
 
 public abstract class BaseErrorHandler implements ErrorHandler {
 
   protected ErrorResponse getErrorResponse(Response response) throws TrusonaException {
-    Reader errorBody = response.errorBody().charStream();
+    Optional<ResponseBody> errorBody = Optional.ofNullable(response.errorBody());
 
-    try {
-      return JacksonConfig.getObjectMapper().readValue(errorBody, ErrorResponse.class);
-    }
-    catch (IOException e) {
-      throw new TrusonaException("An unknown error occurred. Contact Trusona to determine the exact cause.", e);
-    }
-    finally {
+    if (errorBody.isPresent()) {
       try {
-        errorBody.close();
+        return JacksonConfig.getObjectMapper().readValue(errorBody.get().charStream(), ErrorResponse.class);
       }
       catch (IOException e) {
+        throw new TrusonaException("An unknown error occurred. Contact Trusona to determine the exact cause.", e);
       }
+      finally {
+        errorBody.get().close();
+      }
+    }
+    else {
+      return new ErrorResponse("UNKNOWN", "An unknown error occurred. Contact Trusona to determine the exact cause");
     }
   }
 }
