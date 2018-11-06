b5e11ba5f5e07ac0d5195108cda66e8ee90bfa69
Nikolas Thitu <nikolas@hauz.io>
2018-11-06 13:03:01 -0500

    fixing bash

--- bin/version.sh
+++ bin/version.sh
@@ -16,15 +16,15 @@ VERSION=$1
 
 if [ ! -f build.gradle ]; then
   echo "Cannot find build.gradle. Make sure you run this from the root of the repo."
-  exit
+  exit -1
 fi
 
 if [[ "${VERSION}" =~ ^[0-9]*\.[0-9]*\.[0-9]*$ ]]; then
   echo "Setting version to ${VERSION}"
 else
   echo "We use Semantic Versioning. Please use the format X.Y.Z"
-  exit
+  exit -2
 fi
 
-sed -i '' "s/[0-9]*\.[0-9]*\.[0-9]*-SNAPSHOT/${VERSION}-SNAPSHOT/" build.gradle
-sed -i '' "s/com.trusona:trusona-sdk:[0-9]*\.[0-9]*\.[0-9]*/com.trusona:trusona-sdk:${VERSION}/" README.md
+sed -i "s/[0-9]*\.[0-9]*\.[0-9]*-SNAPSHOT/${VERSION}-SNAPSHOT/" build.gradle
+sed -i "s/com.trusona:trusona-sdk:[0-9]*\.[0-9]*\.[0-9]*/com.trusona:trusona-sdk:${VERSION}/" README.md
