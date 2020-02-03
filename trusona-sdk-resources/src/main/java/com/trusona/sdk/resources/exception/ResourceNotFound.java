42213dcb5fdc0e475c8b81e610b94060f2d9a709
Nikolas Thitu <nikolas@hauz.io>
2020-02-03 12:18:19 -0500

    exception for 404

---
+++ trusona-sdk-resources/src/main/java/com/trusona/sdk/resources/exception/ResourceNotFound.java
@@ -0,0 +1,10 @@
+package com.trusona.sdk.resources.exception;
+
+public class ResourceNotFound extends TrusonaException {
+
+  private static final long serialVersionUID = 7772882186088602409L;
+
+  public ResourceNotFound(String message) {
+    super(message);
+  }
+}
