5aec5d5d32d7b30f9cb14256130dd7c2fca88224
Nikolas Thitu <nikolas@hauz.io>
2020-02-03 12:18:27 -0500

    exception for 422

---
+++ trusona-sdk-resources/src/main/java/com/trusona/sdk/resources/exception/ResourceNotProcessable.java
@@ -0,0 +1,10 @@
+package com.trusona.sdk.resources.exception;
+
+public class ResourceNotProcessable extends TrusonaException {
+
+  private static final long serialVersionUID = 979016294537540040L;
+
+  public ResourceNotProcessable(String message) {
+    super(message);
+  }
+}
