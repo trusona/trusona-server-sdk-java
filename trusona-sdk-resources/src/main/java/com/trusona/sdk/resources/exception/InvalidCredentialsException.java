75007fa0e99d685c06bf0007680b6ee277983bc6
Nikolas Thitu <nikolas@hauz.io>
2020-02-03 12:18:39 -0500

    exceptin for 403

--- trusona-sdk-resources/src/main/java/com/trusona/sdk/resources/exception/InvalidCredentialsException.java
+++ trusona-sdk-resources/src/main/java/com/trusona/sdk/resources/exception/InvalidCredentialsException.java
@@ -1,9 +1,10 @@
 package com.trusona.sdk.resources.exception;
 
-public class InvalidCredentialsException extends RuntimeException {
+public class InvalidCredentialsException extends TrusonaException {
+
   private static final long serialVersionUID = -5826997581995153704L;
 
-  public InvalidCredentialsException(String message) {
-    super(message);
+  public InvalidCredentialsException() {
+    super("The token and/or secret you are using are invalid. Contact Trusona to get valid Server SDK credentials.");
   }
 }
