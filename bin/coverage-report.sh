66db783c88a95775643adc1598b00ddbc76833ec
John M <jm@trusona.com>
2020-06-26 07:42:18 -0700

    cleaned up coverage-report.sh script

--- bin/coverage-report.sh
+++ bin/coverage-report.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+function setup_cc {
+  if [ ! -f ./cc-test-reporter ]; then
+
+    os=darwin
+    if [[ -n $TRAVIS ]]; then
+      os=linux
+    fi
+
+    curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-${os}-amd64 > ./cc-test-reporter
+    chmod +x ./cc-test-reporter
+  fi
+}
+
 function coverage {
     name=$1
 
@@ -7,18 +20,22 @@ function coverage {
     report_path=${name}/build/reports/jacoco/test/jacocoTestReport.xml
     out_path=coverage/codeclimate.${name}.json
 
-    echo JACOCO_SOURCE_PATH=${source_path} ./cc-test-reporter format-coverage ${report_path} --input-type jacoco -o ${out_path}
+    JACOCO_SOURCE_PATH=${source_path} ./cc-test-reporter format-coverage ${report_path} --input-type jacoco -o ${out_path}
 }
 
-curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
-chmod +x ./cc-test-reporter
+function publish {
+  ./cc-test-reporter sum-coverage coverage/codeclimate.*.json -p 3
+
+  if [[ "$TRAVIS_TEST_RESULT" == 0 ]]; then
+    ./cc-test-reporter upload-coverage
+  fi
+}
+
+
+setup_cc
 
 coverage trusona-sdk
 coverage trusona-sdk-http
 coverage trusona-sdk-resources
 
-./cc-test-reporter sum-coverage coverage/codeclimate.*.json -p 3
-
-if [[ "$TRAVIS_TEST_RESULT" == 0 ]]; then
-  ./cc-test-reporter upload-coverage
-fi
+publish
