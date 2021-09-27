7aad317a1e52ebe457b9e151ed50859d07a3bbe4
Nikolas Thitu <nikolas@hauz.io>
2021-09-27 14:43:29 -0400

    use travis slug to determine if it's a trusona based run

--- bin/ci-setup-env.sh
+++ bin/ci-setup-env.sh
@@ -12,6 +12,13 @@ echo "signingKeyId=${SIGNING_KEY_ID}" >> ~/.gradle/gradle.properties
 echo "signingPassword=${SIGNING_PASSWORD}" >> ~/.gradle/gradle.properties
 echo "signingSecretKeyRingFile=${SIGNING_KEY_RING_FILE}" >> ~/.gradle/gradle.properties
 
-if [ -n "$TRAVIS_TAG" ]; then
+TRUSONA=
+
+if [[ "$TRAVIS_PULL_REQUEST_SLUG" =~ ^trusona\/.*$ ]] || [[ "$TRAVIS_REPO_SLUG" =~ ^trusona\/.*$ ]]; then
+  export TRUSONA=1
+  TRUSONA=1
+fi
+
+if [ -n "$TRUSONA" ]; then
   openssl aes-256-cbc -K $encrypted_877b689a489a_key -iv $encrypted_877b689a489a_iv -in keystores/trusona-signing.gpg.enc -out keystores/trusona-signing.gpg -d
 fi
